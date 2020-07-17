2523e41d0b8f53cddf65b9e71f9fceda0ca52bcb
Shelley Brook <sbrook13@gmail.com>
2020-07-17 03:55:48 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,23 @@
+def using_concat(x,y)
+  x.concat(y)
+end
+
+def using_insert(a,b)
+  a.insert(4,b)
+end
+
+def using_uniq(array)
+  array.uniq 
+end
+
+def using_flatten(array)
+  array.flatten 
+end
+
+def using_delete (array, string)
+  array.delete(string)
+end
+
+def using_delete_at(array, num)
+  array.delete_at(num)
+end  
